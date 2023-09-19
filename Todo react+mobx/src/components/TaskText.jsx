f28052024cb7084a88f7ddf13e592bbe0f95f3fa
ArtemSyulatov <artem.syulatov.11111@gmail.com>
2023-09-19 16:16:01 +0500

    styled button,textarea

--- Todo react+mobx/src/components/TaskText.jsx
+++ Todo react+mobx/src/components/TaskText.jsx
@@ -1,11 +1,11 @@
 import { observer } from "mobx-react-lite";
-import React from "react";
+import React from "react"; 
 
 const TaskText = observer((task) => {
   console.log(task);
   return (
     <div> 
-      <p></p>
+      <p>Здесь будет тайтл задачи</p>
       <textarea></textarea>
     </div>
   );
