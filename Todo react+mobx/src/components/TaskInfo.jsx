cbf9fa7ba18d2bc41e2aae6034d8e2a29d306b0d
ArtemSyulatov <artem.syulatov.11111@gmail.com>
2023-09-28 22:52:11 +0500

    add disable for textarea

--- Todo react+mobx/src/components/TaskInfo.jsx
+++ Todo react+mobx/src/components/TaskInfo.jsx
@@ -1,21 +1,29 @@
-import { observer } from "mobx-react-lite";
+import {observer} from "mobx-react-lite";
 import React from "react";
-import { useStore } from "../store/store.js";
+import {useStore} from "../store/store.js";
 
 const TaskInfo = observer(() => {
-  const { todo } = useStore(); 
-  if(!todo.selectedTask){
-    return null
-  }
-  return (
-    <div>
-
-      <p>{todo.selectedTask?.text}</p>
-      <textarea value = {todo.selectedTask?.title} onChange={(e) => {
-          todo.selectedTask.setTitle(e.target.value)
-      }}></textarea>
-    </div>
-  );
+    const {todo} = useStore();
+    if (!todo.selectedTask) {
+        return null
+    }
+    return (
+        <div>
+            <p style={{
+                textDecoration: todo.selectedTask.isChecked ? 'line-through' : 'none',
+                opacity: todo.selectedTask.isChecked ? '0.5' : '1'
+            }}
+            >{todo.selectedTask?.text}</p>
+            <textarea style={{
+                textDecoration: todo.selectedTask.isChecked ? 'line-through' : 'none',
+                opacity: todo.selectedTask.isChecked ? '0.5' : '1'
+            }} value={todo.selectedTask?.title} onChange={(e) => {
+                todo.selectedTask.setTitle(e.target.value)
+            }}
+             disabled={!!todo.selectedTask.isChecked}
+            ></textarea>
+        </div>
+    );
 });
 
 export default TaskInfo;
